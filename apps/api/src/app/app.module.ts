ede87599186abf76733b15c3584c5a601529edfc
Bram Borggreve <borggreve@gmail.com>
2020-08-07 00:09:03 -0500

    add NestJS api app and core library

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { CoreModule } from '@beehive/core'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+@Module({
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
